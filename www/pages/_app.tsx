500dfdc15602185ebe6c7ca1736d75a5d4fc0c41
Efra Peralta <efra23peralta@gmail.com>
2023-01-03 17:26:27 -0400

    chore: remove telemetry code and add trailing slash

--- www/pages/_app.tsx
+++ www/pages/_app.tsx
@@ -2,34 +2,10 @@ import { APP_NAME, DESCRIPTION } from 'lib/constants'
 import { DefaultSeo } from 'next-seo'
 import { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
 import Meta from '~/components/Favicons'
 import '../styles/index.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-
-  useEffect(() => {
-    // `routeChangeComplete` won't run for the first page load unless the query string is
-    // hydrated later on, so here we log a page view if this is the first render and
-    // there's no query string
-    if (!router.asPath.includes('?')) {
-      //telemetry()
-    }
-  }, [])
-
-  useEffect(() => {
-    function handleRouteChange() {
-      //telemetry()
-    }
-
-    // Listen for page changes after a navigation or when the query changes
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-
   const site_title = `The Open Source Twilio Alternative | ${APP_NAME}`
   const { basePath } = useRouter()
 
